ca3d9a9e2b400ab1f6c0645e1be7862a66890c2a
Shahnab <ahmedshahnab@gmail.com>
2023-05-26 22:34:02 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+st. set_page_config(layout="wide")
+
+# embed streamlit docs in a streamlit app
+st.components.v1.iframe("https://shad0ws-traffic.hf.space", width=1500, height=800, scrolling=True)
+
